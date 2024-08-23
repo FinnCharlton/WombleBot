4cab098c1a6fe16aab863be91dfffd7b7505ec18
Finn Charlton <finn.charlton@hotmail.com>
2024-08-23 11:23:17 +0100

    volume tests

--- tts_test.py
+++ tts_test.py
@@ -2,7 +2,7 @@ import pyttsx3
 
 try:
     engine = pyttsx3.init()
-    engine.setProperty('volume',1.0)
+    engine.setProperty('volume',10.0)
     engine.setProperty('rate', 175)
     engine.say("Text to Speech")
     print("Spoken")
